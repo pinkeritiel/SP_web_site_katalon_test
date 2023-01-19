4f7220bcc04bff1aba7fad6987f43d4a2dcc6796
pinkeritiel <pinkeritiel@gmail.com>
2023-01-19 16:24:47 +0200

    cc

---
+++ Object Repository/Page_Plants Shop/div_Plants.rs
@@ -0,0 +1,83 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>div_Plants</name>
+   <tag></tag>
+   <elementGuidId>b6b46804-bf57-458e-98c2-83703bd500c4</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>(.//*[normalize-space(text()) and normalize-space(.)='Hello, guest'])[1]/preceding::div[1]</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>div.headerLeft > div</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>div</value>
+      <webElementGuid>8baaa57f-da90-49da-a834-6fe24a56493b</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Plants</value>
+      <webElementGuid>b5128872-a54e-4290-a0b0-38bb0884a5cc</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>/html[1]/body[1]/app-root[1]/div[@class=&quot;container&quot;]/app-header[1]/div[@class=&quot;headerContainer container&quot;]/div[@class=&quot;upperHeader&quot;]/div[@class=&quot;headerLeft&quot;]/div[1]</value>
+      <webElementGuid>8cdaee8a-e25a-443c-aacd-461258de764f</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Hello, guest'])[1]/preceding::div[1]</value>
+      <webElementGuid>18d30a73-be2c-48f9-b4e1-ab7799f77dd7</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Login As Guest'])[1]/preceding::div[4]</value>
+      <webElementGuid>aa460b2e-4d2c-4234-99f2-ad0f3980b26a</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>//*/text()[normalize-space(.)='Plants']/parent::*</value>
+      <webElementGuid>69cbe136-3067-471f-9fc5-5f9c85919a3a</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div/div/div/div</value>
+      <webElementGuid>8c26dab4-4639-474d-84de-5f77593b25e7</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//div[(text() = 'Plants' or . = 'Plants')]</value>
+      <webElementGuid>5b40ab46-8c67-4b50-9e96-4f61bb7855ad</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
---
+++ Test Suites/Shirel Site tests.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Shirel Site tests</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>e86e4dbd-d0b3-4cee-b05c-d9e4b60b6dc0</testSuiteGuid>
+   <testCaseLink>
+      <guid>462f43b2-8fff-49aa-803b-62b138995bc4</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Check that Plants exist</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
